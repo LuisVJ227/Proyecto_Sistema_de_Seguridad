bcf1b00cd96d0227c746572d9208b998dd920972
LuisVJ227 <106263975+LuisVJ227@users.noreply.github.com>
2022-09-05 07:59:59 -0500

    Update ProcesoenPYTHON.py

--- ProcesoenPYTHON.py
+++ ProcesoenPYTHON.py
@@ -1,16 +1,19 @@
-import time
-import smtplib
-import serial
 from tkinter import *
+import time 
+import serial
+import smtplib
 from email.message import EmailMessage
+import threading
 
-Puerto = serial.Serial("COM5",9600)
+Arduino = serial.Serial('COM5', 9600)
+time.sleep(0.5)
+
 
 def EnviarCorreo():
 	#Declaro el correo y contraseña
     Emisor = "luisalbertodejesusv883@gmail.com"
     Contrasena = "qfurqdaeaamepcer"
-    Receptor = "t1513600321@unitru.edu.pe"
+    Receptor = "t1013600621@unitru.edu.pe"
 
     print("Iniciando envio")
     #Obteniendo la fecha y hora actual 
@@ -35,62 +38,67 @@ def EnviarCorreo():
         smtp.login(Emisor, Contrasena)
         smtp.sendmail(Emisor, Receptor, em.as_string())
 
-def cerrarInterfaz():
-    Puerto.close()
-    control.destroy()
+cont = 0
 
-def encenderSIST():
-    Puerto.write('a'.encode())
-    time(1)
+def llamar_sistema():
+    global cont
     while True:
-        while(Puerto.inWaiting()==0):
-            pass
-            Dato = Puerto.readline()
-            #Para ver los datos que arroja
-            #print(Dato)
-            a = Dato.splitlines()
-            b = str(a[0])
-            c = b.replace("b","")
-            d = c.replace("'","")
-            e = int(d)
-            print (e)
-
-            if (e == 0):
+        time.sleep(1)
+        val = Arduino.readline()
+        val = val.decode()
+        #Para ver los datos que arroja
+        a = int(val)
+        print(a)
+        if (a == 0):
+            cont =+1
+            if (cont <= 1):
                 EnviarCorreo()
                 print('ALERTA')
-                time.sleep(0.5)
-            time.sleep(0.1)
-   
-def apagarSIST():
-    Puerto.write('b'.encode())
-    time(1)
+        elif (a == 1):
+            cont = 0
+
+def encender():
+    Arduino.write('a'.encode())
+    time.sleep(0.1)
+
+def apagar():
+    Arduino.write('b'.encode())
+    time.sleep(0.1)
+
+
+def cerrarInterfaz():
+    Arduino.close()
+    control.destroy()
+
+
+threading.Thread(target=llamar_sistema).start()
 
 control = Tk()
 
 control.geometry("500x200")
 control.title('control de sistema')
 
-titleFrame = Frame()
-titleFrame.config(bg = "yellow", width = "500", height = "80")
-titleFrame.place(x=0, y=0)
-#titulo
-lbltitulo = Label(titleFrame, text = "ENCENDIDO Y APAGADO DE SISTEMA", bg = "yellow", font =("ARIAL", 15))
-lbltitulo.place(x=70, y=20)
 
-botonesFrame = Frame()
-botonesFrame.config(bg = "orange", width = "500", height = "120")
-botonesFrame.place(x=0, y=80)
+boton_1 = Label(control,  bg = 'green')
+boton_1.place(x=20,y=50)
+
+boton_2 = Label(control, bg = 'red')
+boton_2.place(x=250,y=50)
+
+boton_3 = Label(control, bg = 'red')
+boton_3.place(x=400,y=150)
 
 #boton de encender
-encender = Button(botonesFrame, text = "ACTIVAR", bg = "green", fg = "white", font = ("ARIAL", 12),command = encenderSIST)
-encender.place(x=100, y=40)
+encender_c = Button(boton_1, text = "ACTIVAR", bg = "white", fg = "black", font = ("ARIAL", 20), command = lambda:encender())
+encender_c.pack()
 
 #boton de apagar
-apagar = Button(botonesFrame, text = "APAGAR", bg = "red", fg = "white", font = ("ARIAL", 12),command = apagarSIST)
-apagar.place(x=300, y=40)
+apagar_c = Button(boton_2, text = "APAGAR", bg = "white", fg = "black", font = ("ARIAL", 20), command = lambda:apagar())
+apagar_c.pack()
 
-#boton de Cerrar sistema
-cerrar= Button(botonesFrame, text = ("Salir"),font = ("ARIAL", 14), command = cerrarInterfaz)
-cerrar.place(x=440, y=80)
+#boton de cerrar
+cerrar_c = Button(boton_3, text = "CERRAR", bg = "white", fg = "black", font = ("ARIAL", 10), command = lambda:cerrarInterfaz())
+cerrar_c.pack()
+
 
 control.mainloop()
